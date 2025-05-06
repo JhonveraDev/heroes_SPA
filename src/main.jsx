87321c9685d9c581414c13872aeee6f7f5fec85a
Jhon Vera <seguridad.veracontainer@gmail.com>
2025-05-05 19:44:14 -0500

    Update Version

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HeroesApp } from './HeroesApp';
+import './main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </StrictMode>,
 )
